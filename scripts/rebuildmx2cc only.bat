de2d222694cb942aa63005bc8ddfd7bcbbca195d
SamFisher <christianwilss@gmail.com>
2018-11-23 23:21:20 +0100

    Update mx2cc

---
+++ scripts/rebuildmx2cc only.bat
@@ -0,0 +1,16 @@
+
+echo off
+
+call common.bat
+
+echo.
+echo ***** Rebuilding mx2cc *****
+echo.
+
+%mx2cc% makeapp -verbose -apptype=console -config=release -product=scripts/mx2cc.products/mx2cc_windows.exe ../src/mx2cc/mx2cc.monkey2
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+copy mx2cc.products\mx2cc_windows.exe %mx2cc%
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+pause
